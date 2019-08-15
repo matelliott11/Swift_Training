bbda70f1bdd2513829fd5cf2f291e655fb832679
matthew elliott <mat.elliott.11@gmail.com>
2019-08-15 23:17:05 +0100

    added Find Movie Project
    added Find Movie Project

---
+++ favorite-movies-app/favorite-movies-app/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  favorite-movies-app
+//
+//  Created by matt elliott on 24/07/2019.
+//  Copyright © 2019 matt elliott. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
